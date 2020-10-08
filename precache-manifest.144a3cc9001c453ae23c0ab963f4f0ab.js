bf9f6e2203b9849dd0cd12d5817898988e54ca5f
rosaerosa <edith.spoordonk@gmail.com>
2020-10-08 11:59:52 +0200

    Updates

---
+++ precache-manifest.144a3cc9001c453ae23c0ab963f4f0ab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e6cf52cb6cd65b19a34a8ccc28c6468",
+    "url": "/magiccamera/index.html"
+  },
+  {
+    "revision": "21f77391d14c7f8dd472",
+    "url": "/magiccamera/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "24056b09247f6d8aad45",
+    "url": "/magiccamera/static/css/main.e28bc378.chunk.css"
+  },
+  {
+    "revision": "21f77391d14c7f8dd472",
+    "url": "/magiccamera/static/js/2.0cb27001.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/magiccamera/static/js/2.0cb27001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24056b09247f6d8aad45",
+    "url": "/magiccamera/static/js/main.1a2f708e.chunk.js"
+  },
+  {
+    "revision": "fa6ad3acb36a72467641",
+    "url": "/magiccamera/static/js/runtime-main.c861d7c1.js"
+  },
+  {
+    "revision": "75af5514334743015cf68549e2bc7508",
+    "url": "/magiccamera/static/media/Citrica-Regular.75af5514.woff"
+  },
+  {
+    "revision": "b2804135367f39e3ec85dffa0b9372b9",
+    "url": "/magiccamera/static/media/fotos.b2804135.jpg"
+  }
+]);
